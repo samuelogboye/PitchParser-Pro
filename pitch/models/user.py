9b4e6897f6ad420aa2f4199c1041904f3dd4cb67
Samuel Ogboye <ogboyesam@gmail.com>
2025-03-26 14:42:28 +0100

    feat: implement user model

---
+++ pitch/models/user.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from pitch.models.base import BaseModel
+from time import time
+
+db = SQLAlchemy()
+
+class User(BaseModel):
+    """User model for authentication"""
+    __tablename__ = 'users'
+
+    updated_at = None
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    refresh_tokens = db.relationship('RefreshToken', backref='user', lazy=True)
+    
+    # Relationship to pitch decks
+    pitch_decks = db.relationship('PitchDeck', backref='owner', lazy='dynamic')
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        db.session.commit()
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def format(self):
+        '''Return a dictionary representation of the user object'''
+        return {
+            'id': self.id,
+            'email': self.email,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
+    
+
+class RefreshToken(BaseModel):
+    '''Refresh Token Table'''
+    __tablename__ = 'refreshtokens'
+
+    token = db.Column(db.String(256), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    used = db.Column(db.Boolean, default=False, nullable=False)
+    expires_at = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        '''Return a string representation of the refresh token object'''
+        return f"RefreshToken('{self.token}', '{self.user_id}', '{self.expires_at}')"
+
+    def format(self):
+        '''Return a dictionary representation of the refresh token object'''
+        return {
+            'token': self.token,
+            'user_id': self.user_id,
+            'used': self.used,
+            'expires_at': self.expires_at
+        }
