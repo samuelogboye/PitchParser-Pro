7f7ffd9b131c1c0e25680d4e180436b59f79d2e8
Samuel Ogboye <ogboyesam@gmail.com>
2025-03-26 10:11:39 +0100

    feat: implement validation utils

---
+++ pitch/utils/validation.py
@@ -0,0 +1,14 @@
+
+'''Validate inputs'''
+import re
+
+def validate_email(email):
+    ''' Validate Email address'''
+    email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return re.match(email_regex, email) is not None
+
+def validate_password(password):
+    ''' Validate Password'''
+    # Example: Minimum 8 characters, at least one letter and one number
+    password_regex = r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$'
+    return re.match(password_regex, password) is not None
